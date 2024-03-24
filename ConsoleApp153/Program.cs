f7e54429236dd6c23542052eb2741f7a052b7fcf
ygursu <yasemingrs@gmail.com>
2024-03-24 23:56:40 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp153/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        int a, b;
+
+        Console.Write("İlk sayı :  ");
+        int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("İkinci sayı :  ");
+        int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i < 100; i++)
+        {
+            a = i % sayi1;
+            b = i % sayi2;
+            if (a == 0 && b == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+}
